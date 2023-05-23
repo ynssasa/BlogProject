3c80458693ef851872cc80cbc8abc1018937a1df
Yunus SASA <sasayunus03@gmail.com>
2023-05-23 21:33:49 +0300

    GenericRepository added

---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        Context context = new Context();
+        public void Add(T item)
+        {
+            context.Add(item);
+            context.SaveChanges();
+        }
+
+        public void Delete(T item)
+        {
+            context.Remove(item);
+            context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public void Update(T item)
+        {
+            context.Update(item);
+            context.SaveChanges();
+        }
+    }
+}
