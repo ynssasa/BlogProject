3408c699c300c9a6c03a676d817af0c2ea42a984
Yunus SASA <sasayunus03@gmail.com>
2023-05-23 22:08:11 +0300

    EfRepository added

--- DataAccessLayer/Abstract/ICategoryDal.cs
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -7,12 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface ICategoryDal
-    {
-        List<Category> GetAll();
-        void Add(Category category);
-        void Delete(Category category);
-        void Update(Category category);
-        Category GetById(int id);
+    public interface ICategoryDal:IGenericDal<Category>
+    {     
     }
 }
