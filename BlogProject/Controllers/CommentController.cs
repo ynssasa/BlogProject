94d4d884f55d966280d0eb9f68d95d641a49b765
Yunus SASA <sasayunus03@gmail.com>
2023-05-26 14:27:50 +0300

    CommentController added

---
+++ BlogProject/Controllers/CommentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProjectUI.Controllers
+{
+    public class CommentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialAddComment()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialCommentListByBlog()
+        {
+            return PartialView();
+        }
+    }
+}
